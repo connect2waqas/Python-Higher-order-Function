dd7f5af8dd0648e21d8a31b581bf444ba2cd784d
waqas khan <207045044+connect2waqas@users.noreply.github.com>
2026-03-17 00:59:10 +0500

    Refactor calculator and greeting functions
    We are adding a function using the same logic as before in the code through functional programming and removing unnecessary spaces from the dictionary.

--- Higher Order Functions/lecture2/code.py
+++ Higher Order Functions/lecture2/code.py
@@ -1,4 +1,4 @@
-# Small Project: Desining calculator:
+# Small Project: Designing a calculator:
 
 operation ={
     "-" : lambda a , b : a - b,
@@ -15,37 +15,33 @@ print(f"Result: {operation[operator](num1,num2)}")
 
 import pandas as pd
 people = [
-    {"name": "Alice","age" : 15},
-    {"name": "Bob", "age" : 18},
-    {"name": "Charlie","age": 20}
+    { "name": "Alice", "age": 15},
+    {"name": "Bob", "age": 24},
+    {"name": "Charlie", "age": 50}
 ]
-
-adults = list(filter(lambda p : p["age"] >= 18, people))
-print(adults)
-
+adults = list(filter(lambda x : x["age"] >=18, people))
 df = pd.DataFrame(adults)
-
-output = df.to_csv("output.csv")
-print(output)
-# Temprature Converter:
+output_data = df.to_csv("output.csv")
+print(output_data)
+# Temperature Converter:
 
 c_to_f = lambda c : (c * 9 / 5) + 32
 print(c_to_f(34))
 
-import datetime
-
-current_hour = datetime.datetime.now().hour
-
-greeting_rule = {
-    "morning": lambda : "Good Morning! 🌞",
-    "afternoon" : lambda: "Good afternoon! ☀️",
-    "evening"  : lambda: "Good evening! 🌙"
-}
-if 5 <= current_hour < 12:
-    greet = greeting_rule["morning"]
-elif 12 <= current_hour < 17:
-    greet = greeting_rule["afternoon"]
-else:
-    greet = greeting_rule["evening"]
-
-print(greet())
+def greeting():
+    import datetime
+    current_hour = datetime.datetime.now().hour
+    greeting_rule = {
+        "morning": lambda: "Good Morning!",
+        "afternoon" : lambda: "Good afternoon!",
+        "evening"  : lambda: "Good evening!"
+    }
+    if 5 <= current_hour < 12:
+        greet = greeting_rule["morning"]
+    elif 12 <= current_hour < 17:
+        greet =  greeting_rule["afternoon"]
+    else:
+        greet = greeting_rule["evening"]
+    return greet
+greet_1 = greeting()
+print(greet_1)
